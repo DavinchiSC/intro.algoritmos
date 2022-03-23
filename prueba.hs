3a3630dce6b56fe236190c49d56ea6570d95d471
DavinchiSC <david.cortes@mi.unc.edu.ar>
2022-03-22 23:03:23 -0300

    + 1 ejercicio
    ejercicio ordenar tuplas

--- prueba.hs
+++ prueba.hs
@@ -2,3 +2,6 @@
 --divisible por 400 o es divisible por 4 pero no es divisible por 100. 
 esBisiesto :: Int -> Bool
 esBisiesto a = (mod a 400) == 0 || ((mod a 4) == 0 && ((mod a 100) /= 0))
+--ordena : (Num, Num) → (Num, Num), que dados dos enteros los ordena de menor a mayor.
+ordena :: (Int,Int) -> (Int,Int)
+ordena (a,b) = ((min a b),(max a b))
